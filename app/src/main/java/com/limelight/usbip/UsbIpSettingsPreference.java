1014d19201347b12494d1b9eb9d662bbeecbfc2f
Tan90degrees <72683500+Tan90degrees@users.noreply.github.com>
2026-09-07 17:46:04 +0800

    Disable USB/IP settings without USB host support

--- app/src/main/java/com/limelight/usbip/UsbIpSettingsPreference.java
+++ app/src/main/java/com/limelight/usbip/UsbIpSettingsPreference.java
@@ -2,6 +2,7 @@ package com.limelight.usbip;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.preference.Preference;
 import android.util.AttributeSet;
 
@@ -9,15 +10,23 @@ import android.util.AttributeSet;
 public final class UsbIpSettingsPreference extends Preference {
     public UsbIpSettingsPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        updateAvailability(context);
     }
 
     public UsbIpSettingsPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        updateAvailability(context);
+    }
+
+    private void updateAvailability(Context context) {
+        setEnabled(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_USB_HOST));
     }
 
     @Override
     protected void onClick() {
         super.onClick();
-        getContext().startActivity(new Intent(getContext(), UsbIpSettings.class));
+        if (isEnabled()) {
+            getContext().startActivity(new Intent(getContext(), UsbIpSettings.class));
+        }
     }
 }
